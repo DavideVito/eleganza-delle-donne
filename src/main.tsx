c3d150e71a6805e43df304b6d1036d58f0a72b0c
DavideVito <lgvitiello9@gmail.com>
2024-10-19 02:39:17 +0200

    Cambiati colori

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,26 @@ import { ListaClienti } from './Pages/Info/Clienti/ListaClienti/ListaClienti.tsx
 import BottomAppBar from './Components/BottomAppBar/BottomAppBar.tsx'
 import { CreaCliente } from './Pages/Crea/Cliente/CreaCliente.tsx'
 import { InfoCliente } from './Pages/Info/Clienti/InfoCliente/InfoCliente.tsx'
+import { createTheme, ThemeOptions, ThemeProvider } from '@mui/material'
+const themeOptions: ThemeOptions = {
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#de79aa',
+      light: '#E3B9C3', // Optional
+      dark: '#9A7B88',  // Optional
+      contrastText: '#FFFFFF', // Optional
+    },
+    secondary: {
+      main: '#f50057',
+      light: '#ff4081', // Optional
+      dark: '#c51162',  // Optional
+      contrastText: '#FFFFFF', // Optional
+    },
+  },
+};
+
+const theme = createTheme(themeOptions);
 
 const routes = createBrowserRouter([{
   path: "/",
@@ -29,9 +49,11 @@ const routes = createBrowserRouter([{
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div style={{ padding: "1rem" }}>
-      <RouterProvider router={routes} />
-    </div>
-    <BottomAppBar />
+    <ThemeProvider theme={theme}>
+      <div style={{ padding: "1rem" }}>
+        <RouterProvider router={routes} />
+      </div>
+      <BottomAppBar />
+    </ThemeProvider>
   </React.StrictMode>,
 )
